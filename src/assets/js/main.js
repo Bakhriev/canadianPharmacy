892a5b6b38c441987c919f1f1be4fab123677a62
Bakhriev <legendofpain5112@gmail.com>
2023-10-02 16:21:35 +0500

    xd

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,11 +25,13 @@ const burgerMenu = () => {
 burgerMenu()
 
 // For Mobile Dropdown --Start--
-const dropdowns = document.querySelectorAll('.menu')
+const dropdowns = document.querySelectorAll('.menu-item-has-children')
+
 dropdowns.forEach(dropdown => {
 	const subMenu = dropdown.querySelector('.sub-menu')
 	dropdown.addEventListener('click', toggleSubMenu)
 })
+
 function toggleSubMenu() {
 	const subMenu = this.querySelector('.sub-menu')
 	subMenu.style.maxHeight = subMenu.style.maxHeight
